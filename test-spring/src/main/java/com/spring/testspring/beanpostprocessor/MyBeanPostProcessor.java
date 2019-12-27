6bdc6885f6e1d0f946caa676c4af2b13da7ca85d
dengfenwu <dengfenwu@ysstech.com>
2019-12-27 16:14:41 +0800

    通用方法封装

--- test-spring/src/main/java/com/spring/testspring/beanpostprocessor/MyBeanPostProcessor.java
+++ test-spring/src/main/java/com/spring/testspring/beanpostprocessor/MyBeanPostProcessor.java
@@ -8,13 +8,13 @@ import org.springframework.stereotype.Component;
 
 
 /*
-BeanPostProcessor接口的作用是在Spring容器完成Bean实例化前后可以添加一些自己的逻辑处理，我们可以定义一个或者多个BeanPostProcessor接口的实现。
+BeanPostProcessor接口的作用是在Spring容器完成Bean初始化前后可以添加一些自己的逻辑处理，我们可以定义一个或者多个BeanPostProcessor接口的实现。
 
 BeanPostProcessor接口提供了两个方法：
 
-1、postProcessBeforeInitialization  可以对Bean在实例化之前添加一些逻辑处理
+1、postProcessBeforeInitialization  可以对Bean在初始化之前添加一些逻辑处理
 
-2、postProcessAfterInitialization  可以对bean在实例化之后添加一些逻辑处理
+2、postProcessAfterInitialization  可以对bean在初始化之后添加一些逻辑处理
 
 （1）BeanFactoryPostProcessor和普通的bean一样注册到spring容器中。
 （2）获取所有的BeanFactoryPostProcessor类型的类并初始化，添加到列表中
--- test-spring/src/main/java/com/spring/testspring/jdkproxy/MyQueryBeanPostProcessor.java
+++ test-spring/src/main/java/com/spring/testspring/jdkproxy/MyQueryBeanPostProcessor.java
@@ -13,9 +13,14 @@ public class MyQueryBeanPostProcessor implements BeanPostProcessor {
 
 		if(beanName.equals("myInterfacesIml")){
 			//这里bean ”myInterfacesIml“实现了两个接口，下面的索引就代表接口的索引；
-			Class<?> cl = bean.getClass().getInterfaces()[0];
+			/*Class<?> cl = bean.getClass().getInterfaces()[0];
 			Class<?> cl2 = bean.getClass().getInterfaces()[1];
-			Class<?>[] cs = new Class[]{cl,cl2};
+			Class<?>[] cs = new Class[]{cl,cl2};*/
+
+			Class<?>[] cs = new Class[bean.getClass().getInterfaces().length];
+			for (int i=0;i<bean.getClass().getInterfaces().length;i++){
+				cs[i] = bean.getClass().getInterfaces()[i];
+			}
 			//返回一个代理对象
 			Object o = Proxy.newProxyInstance(MyQueryBeanPostProcessor.class.getClassLoader(),cs,new MyInovationHandler(bean));
 			return o;
